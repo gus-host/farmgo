c14196ef244a26edc6ea66bbe70e03da425da7f3
cipher <fynhacker@gmail.com>
2023-09-13 19:06:27 +0100

    new commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv/config");
 
-app.use(cors());
+app.use(cors({ origin: "http://farmgo-backend.farmgo.co.farmgo.co" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
@@ -37,4 +37,4 @@ app.post("/", function (req, res) {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(443);
